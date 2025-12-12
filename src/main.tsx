67376a5e354b8a4ebe71259fd37ba6a649cd188f
David Pilarski <david.pilarski@hotmail.com>
2025-12-12 15:46:42 -0500

    Add automatic update detection

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register'
 import './index.css'
 import App from './App.tsx'
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    updateSW(true)
+  }
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: 'prompt',
       includeAssets: ['vite.svg'],
       manifest: {
         name: 'Vitality Life Counter',
